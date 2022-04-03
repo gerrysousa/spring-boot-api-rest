2d5afe8053dc8f0fb3011e388fc4f82b9bf393d1
Gerry Sousa <gerry@ciandt.com>
2022-04-03 19:10:10 -0300

    07 Removendo tópicos

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -15,6 +15,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class TopicosController {
   }
 
   @PostMapping
+  @Transactional
   public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriComponentsBuilder) {
     Topico topico = form.converter(cursoRepository);
     topicoRepository.save(topico);
@@ -68,6 +70,13 @@ public class TopicosController {
     Topico topico = form.atualizar(id, topicoRepository);
 
     return ResponseEntity.ok(new DetalhesDoTopicoDto(topico));
+  }
+
+  @DeleteMapping("/{id}")
+  @Transactional
+  public ResponseEntity<?> deletar(@PathVariable Long id) {
+    topicoRepository.deleteById(id);
+    return ResponseEntity.ok().build();
   }
 
 }
