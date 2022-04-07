dba91b521cfcb3382cb6e558af7feae1df8b2468
Gerry Sousa <gerry@ciandt.com>
2022-04-07 07:06:39 -0300

    05 Gerando tokens com JWT

---
+++ forum/src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+  @Value("${forum.jwt.expiration}")
+  private String expiration;
+
+  @Value("${forum.jwt.secret}")
+  private String secret;
+
+
+  public String gerarToken(Authentication authentication) {
+    Usuario logado = (Usuario) authentication.getPrincipal();
+    Date agora = new Date();
+    Date expiracao = new Date(agora.getTime() + Long.parseLong(expiration));
+
+    return Jwts.builder()
+        .setIssuer("Api do Forum da Alura")
+        .setSubject(logado.getId().toString())
+        .setIssuedAt(agora)
+        .setExpiration(expiracao)
+        .signWith(SignatureAlgorithm.HS256, secret)
+        .compact();
+  }
+}
