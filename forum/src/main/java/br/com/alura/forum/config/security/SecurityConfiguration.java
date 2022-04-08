036982435e1ccf176c8dba44c706b430011dc229
Gerry Sousa <gerry@ciandt.com>
2022-04-08 07:02:30 -0300

    04 Validando o token

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -22,6 +22,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Autowired
   private AutenticacaoService autenticacaoService;
 
+  @Autowired
+  private TokenService tokenService;
+
   @Override
   @Bean
   protected AuthenticationManager authenticationManager() throws Exception {
@@ -44,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated()
         .and().csrf().disable()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and().addFilterBefore(new AutenticacaoViaTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
   }
 
   //Configuracoes de recursos estaticos (js, css, imagens, etc.)
